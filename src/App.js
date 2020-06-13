c75ea303f37f185d1b0d98a50af8bf72a4d1cc19
kynhat <caokynhat@gmail.com>
2020-06-13 14:18:43 +0700

    update

--- src/App.js
+++ src/App.js
@@ -8,33 +8,8 @@ import Clock from './component/Clock';
 function App() {
   return (
     <div className="App">
-      {/* <Header/>
-      <Body/>
-      <Footer/> */}
-      <Clock/>
-   
-
-     
+      <Clock />
     </div>
   );
 }
-
 export default App;
-
-
-// function tick() {
-//   const element = (
-//     <div>
-//       <h1>Hello, world!</h1>
-//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
-//     </div>
-//   );
-//   ReactDOM.render(
-//     <Clock />,
-//     document.getElementById('root')
-//   );
-// }
-
-// setInterval(tick, 1000);
-
-
